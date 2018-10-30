a45fbbf6920e92d9713783b2073a9dc3bb2d0a82
no1 <carlosmpvaldes@gmail.com>
2018-10-29 21:44:45 -0300

    Cliente simples

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/restfulapi/models"
 	"github.com/restfulapi/routes"
@@ -10,7 +11,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
-	// r.Use(static.Serve("/view", static.LocalFile("client", true)))
+	r.Use(static.Serve("/view", static.LocalFile("client", true)))
 
 	db := models.GetDB()
 	models.Migrate(db)
